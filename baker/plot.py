c109d4a05e44fa9149eb2dda8da0a53f4d5b6ea4
sebaherrera <sebaherrera93@gmail.com>
2018-05-26 03:06:18 -0300

    Add std deviation step

--- baker/plot.py
+++ baker/plot.py
@@ -1,21 +1,21 @@
 import matplotlib.pyplot as plt
 import csv
 
-with open('results/step2_results_la_republica.csv', 'rt', encoding="utf8") as csvfile:
+results_path = "results/step3_results_la_republica.csv"
+with open(results_path, 'rt', encoding="utf8", newline='') as csvfile:
     reader = csv.DictReader(csvfile)
-    y_total_news = []
+    y_epu_news = []
     x_months = []
     x_ticks = []     
     contador = 0  
     for row in reader:
-        y_total_news.append(float(row['epu_news_rel']))
-        x_months.append(row['date'])  
-        x_ticks.append(contador)  
+        y_epu_news.append(float(row['epu_news_std']))
+        x_months.append(row['date'])
+        x_ticks.append(contador)
         contador += 1
 
     print ("genero la grafica \n")
-    # y_total_news.sort()
-    plt.plot(x_months , y_total_news)             
+    plt.plot(x_months , y_epu_news, 'bo')             
     plt.title('EPU para ' + row['newspaper'])
     plt.axis(["10-2011","10-2014", 0, 0.015])
     plt.show()
--- baker/results_processor.py
+++ baker/results_processor.py
@@ -28,10 +28,25 @@ def scale_to_unit_standard_deviation(newspaper):
     previous_results_path = "results/step2_results_" + newspaper + ".csv"
     filepath = "results/step3_results_" + newspaper + ".csv"
     results_handler.create_step3_results_file(newspaper)
-    matrix = np.loadtxt(open(previous_results_path, "rb"), delimiter=",", usecols=(2,3,4,5,6,7,8,9,10,11,12,13,14,15), skiprows=1)
-    #TODO: a partir de los resultados estos se debe hayar la desviacion estandar y hacer la division
-    print(matrix)
-
+    step2_matrix = np.loadtxt(open(previous_results_path, "rb"), delimiter=",", usecols=(2,3,4,5,6,7,8,9,10,11,12,13,14,15), skiprows=1)
+    categories_count = len(step2_matrix[0])
+    std_dev_dict = {}
+    for i in range(categories_count):
+        step2_index = step2_matrix[:,i]
+        std_dev = np.std(step2_index, dtype=np.float64)
+        std_dev_dict[i] = std_dev
+    with open(previous_results_path, newline='') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
+        next(csvreader) # Skips headers row
+        for row in csvreader:
+            date = row[1]
+            data = [newspaper, date, (row[2]/std_dev_dict[0]), (row[3]/std_dev_dict[1]), (row[4]/std_dev_dict[2]), (row[5]/std_dev_dict[3]), 
+                    (row[6]/std_dev_dict[4]), (row[7]/std_dev_dict[5]), (row[8]/std_dev_dict[6]), (row[9]/std_dev_dict[7]), 
+                    (row[10]/std_dev_dict[8]), (row[11]/std_dev_dict[9]), (row[12]/std_dev_dict[10]),  (row[13]/std_dev_dict[11]), 
+                    (row[14]/std_dev_dict[12]), (row[15]/std_dev_dict[13])]
+            with open(filepath, 'a', newline='') as data_file:
+                wr = csv.writer(data_file, quoting=csv.QUOTE_NONNUMERIC)
+                wr.writerow(data)
 
 def get_initial_date():
     with open('date_ranges.json') as data_file:
@@ -118,7 +133,3 @@ def generate_epu_index():
             wr.writerow(data)
 
         current_date = add_one_month(current_date)
-
-
-# MAIN
-generate_epu_index()
